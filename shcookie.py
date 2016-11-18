20d73be08fca46c5987476d3ac228ee3e2073957
Christian Holz <christian@christianholz.net>
2016-11-17 22:11:59 -0800

    cookie parser script

---
+++ shcookie.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""verifies existence of cookie and otherwise shows redirect"""
+
+import os
+import cgi
+import shuttle
+import Cookie
+
+
+k = None
+try:
+  cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
+  k = cookie["skey"].value
+  u, p = shuttle.extract_credentials(k)
+except:
+  print "Status: 302 Moved"
+  print "Location: cookiegen.py"
+  print ""
+  print "cookie missing... redirecting."
+  sys.exit()
