f9f8a321843419eb53008c7198524996ae5082c5
Christian Holz <cholz@microsoft.com>
2017-03-15 10:37:43 -0700

    provide backend URLs

--- shconstants.py
+++ shconstants.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
-
-"""constants to use for backend"""
+'''constants to use backend'''
 
 
 USER_AGENT  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
-ENCRYPT_KEY = 'INSERT_KEY'
-GKEY        = 'INSERT_GOOGLE_MAPS_KEY'
+ENCRYPT_KEY = 'INSERT_KEY_HERE_################################################'
+GKEY        = 'INSERT_GMAPS_KEY_HERE_#################'
 
-URL_MOBILE  = 'https://MOBILE_URL'
-URL_DESKTOP = 'https://DESKTOP_URL'
+URL_MOBILE          = 'https://www.connectorride.mobi'
+URL_DESKTOP         = 'https://www.connectorride.com'
 
-URL_LOGIN_MOBILE   = "https://MOBILE_URL"
-URL_LOGIN_DESKTOP  = "https://MOBILE_URL"
-URL_FLEX_MY_MOBILE = "https://MOBILE_URL"
-URL_FLEX_MOBILE    = "https://MOBILE_URL"
-URL_FLEX_DESKTOP   = "https://MOBILE_URL"
+URL_LOGIN_MOBILE    = 'https://www.connectorride.mobi/Account/Login'
+URL_LOGIN_DESKTOP   = 'https://www.connectorride.com/Account/Login'
+URL_FLEX_MY_MOBILE  = 'https://www.connectorride.mobi/Flex/MyFlex'
+URL_FLEX_MOBILE     = 'https://www.connectorride.mobi/Flex'
+URL_FLEX_DESKTOP    = 'https://www.connectorride.com/Flex/oneTimeFlex'
 
-URL_RETURN_MOBILE  = "https://MOBILE_URL"
-URL_CANCEL_MOBILE  = "https://MOBILE_URL"
+URL_RETURN_MOBILE   = 'https://www.connectorride.mobi/Flex/ReturnOneTimeFlexPartial'
+URL_CANCEL_MOBILE   = 'https://www.connectorride.mobi/Flex/CancelFlex'
 
-URL_RETURN_DESKTOP = "https://DESKTOP_URL"
+URL_RETURN_DESKTOP  = 'https://www.connectorride.com/Flex/ReturnOneTimeFlexPartial'
 
-URL_BOOK_DESKTOP   = "https://DESKTOP_URL"
-URL_DOUBLE_DESKTOP = "https://DESKTOP_URL"
+URL_BOOK_DESKTOP    = 'https://www.connectorride.com/Flex/BookFlex'
+URL_DOUBLE_DESKTOP  = 'https://www.connectorride.com/Flex/BookFlexDoubleBook'
 
-URL_OUTLOOK_DESKTOP = "https://DESKTOP_URL"
+URL_OUTLOOK_DESKTOP = 'https://www.connectorride.com/Flex/ReturnOutlookLink'
 
 MAP_W = 200
 MAP_H = 100
-STOPS_DIR = "stops"
+STOPS_DIR = 'stops'
